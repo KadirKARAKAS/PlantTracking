bd99b19bc0070ff3ac7de597bc8eafc16752d1f0
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2023-07-29 20:34:50 +0300

    finish

--- lib/addPlantsPage/Widget/add_plants_page_textfield.dart
+++ lib/addPlantsPage/Widget/add_plants_page_textfield.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:plant_tracking/Utils/constant.dart';
 import 'package:plant_tracking/addPlantsPage/Widget/plant_image_container_widget.dart';
 import 'package:plant_tracking/plantsHomePage/Page/plants_home_page.dart';
